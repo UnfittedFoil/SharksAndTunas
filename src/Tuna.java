5b165162e7c27f61903ae1f7b2331c75e1a4c49c
Unknown <Starport592@gmail.com>
2018-02-07 12:18:14 -0500

    Minor Constructor Changes
    Updated how the constructor of these objects work in order to conform to the Fish object's new constructor.

--- src/Tuna.java
+++ src/Tuna.java
@@ -7,7 +7,7 @@ public class Tuna extends Fish{
 	private final boolean DEBUG = true;
 	//Line of Sight: 1 degree of indirection from where the fish currently is.  In other words, the shark checks all adjacent cells.
 	
-	public Tuna(int x, int y) {
-		super(x, y, "T");
+	public Tuna() {
+		super("T");
 	}	
 }
