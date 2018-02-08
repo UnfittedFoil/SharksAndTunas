25ddf286ec3cee6d7b676e0a11cc4a7635e70b9a
Matthew WaiMing Tam <Starport592@gmail.com>
2018-02-07 22:40:49 -0500

    Comments denoting constructors in Shark and Tuna class

--- src/Shark.java
+++ src/Shark.java
@@ -12,6 +12,7 @@ public class Shark extends Fish{
 	private final boolean DEBUG = true;  //Debug flag.  Must be manually set.  Enables debug messages to the console.
 	//Line of Sight: 1 degree of indirection from where the fish currently is.  In other words, the shark checks all adjacent cells.
 	
+	//Constructor
 	public Shark() {
 		super("S");
 	}
--- src/Tuna.java
+++ src/Tuna.java
@@ -12,6 +12,7 @@ public class Tuna extends Fish{
 	private final boolean DEBUG = true;
 	//Line of Sight: 1 degree of indirection from where the fish currently is.  In other words, the shark checks all adjacent cells.
 	
+	//Constructor
 	public Tuna() {
 		super("T");
 	}	
